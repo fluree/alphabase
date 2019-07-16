9a47d5f60c092463bd206c6fd599f947332d72c7
Brian Platz <bplatz@flur.ee>
2019-07-15 21:54:45 -0400

    v 3.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fluree/alphabase "3.0.0"
+(defproject fluree/alphabase "3.0.1"
   :description "Clojure(script) library to encode binary data with alphabet base strings."
   :url "https://github.com/fluree/alphabase"
   :license {:name "Public Domain"
