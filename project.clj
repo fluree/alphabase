445b9318f1488858e81b558db4566a478f63f56c
Brian Platz <bplatz@flur.ee>
2019-11-08 16:55:31 -0500

    v 3.2.0

--- project.clj
+++ project.clj
@@ -1,32 +1,32 @@
-(defproject fluree/alphabase "3.1.4"
+(defproject fluree/alphabase "3.2.0"
   :description "Clojure(script) library to encode binary data with alphabet base strings."
   :url "https://github.com/fluree/alphabase"
   :license {:name "Public Domain"
-            :url "http://unlicense.org/"}
+            :url  "http://unlicense.org/"}
 
   :deploy-branches ["master"]
 
   :aliases
-  {"clj:test" ["do" ["check"] ["test"]]
+  {"clj:test"   ["do" ["check"] ["test"]]
    "cljs:check" ["with-profile" "+doo" "cljsbuild" "once"]
-   "cljs:repl" ["run" "-m" "clojure.main" "dev/cljs_repl.clj"]
-   "cljs:test" ["doo" "rhino" "test" "once"]}
+   "cljs:repl"  ["run" "-m" "clojure.main" "dev/cljs_repl.clj"]
+   "cljs:test"  ["doo" "rhino" "test" "once"]}
 
   :plugins
   [[lein-cljsbuild "1.1.7"]
    [lein-doo "0.1.11"]]
 
   :cljsbuild
-  {:builds [{:id "test"
+  {:builds [{:id           "test"
              :source-paths ["src" "test"]
-             :compiler {:optimizations :whitespace
-                        :output-dir "target/cljs/out"
-                        :output-to "target/cljs/tests.js"
-                        :main alphabase.test-runner}}]}
+             :compiler     {:optimizations :whitespace
+                            :output-dir    "target/cljs/out"
+                            :output-to     "target/cljs/tests.js"
+                            :main          alphabase.test-runner}}]}
 
   :codox
-  {:metadata {:doc/format :markdown}
-   :source-uri "https://github.com/greglook/alphabase/blob/master/{filepath}#L{line}"
+  {:metadata    {:doc/format :markdown}
+   :source-uri  "https://github.com/greglook/alphabase/blob/master/{filepath}#L{line}"
    :output-path "target/doc/api"}
 
   :profiles
